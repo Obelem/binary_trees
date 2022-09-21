06fc29eb112cc44dbab05ff0bea3bf68193575cc
Obelem <ibbraidejesse@gmail.com>
2022-09-21 10:09:46 +0100

    removed unwanted NULL check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	if (!parent)
-		return (NULL);
+	binary_tree_t *newNode;
 
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+	newNode = malloc(sizeof(binary_tree_t));
 
 	if (!newNode)
 		return (NULL);
@@ -21,7 +20,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	newNode->left = NULL;
 	newNode->right = NULL;
 	newNode->n = value;
-
 	newNode->parent = parent;
+
 	return (newNode);
 }
