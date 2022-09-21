7e3518758931f142265e1ae4cceb9d18de26501f
Obelem <ibbraidejesse@gmail.com>
2022-09-21 11:51:44 +0100

    initialized new node to NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *leftChild;
+	binary_tree_t *leftChild = NULL;
 
 	if (!parent)
 		return (NULL);
@@ -19,9 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	if (parent->left == NULL)
-	{
 		parent->left = leftChild;
-	}
 	else
 	{
 		leftChild->left = parent->left;
