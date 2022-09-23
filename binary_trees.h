12f5ed5eaf2c6859138f45c643757d000510ff65
Obelem <ibbraidejesse@gmail.com>
2022-09-23 06:03:01 +0100

    added new p prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -40,7 +40,12 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
-
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_perf_check(const binary_tree_t *tree, int bd, int level);
+int bindepth(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 /* driver code prototypes */
 void binary_tree_print(const binary_tree_t *tree);
